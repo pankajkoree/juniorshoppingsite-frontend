8239ed9a65798c7c1ba87c2b771358a542af7d8b
pankajkoree <pankajkori2057@gmail.com>
2026-01-22 19:16:37 +0545

    navigation svg

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -2,7 +2,6 @@
 
 import { Eagle_Lake } from "next/font/google";
 import { Input } from "./ui/input";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const eagle_Lake = Eagle_Lake({
@@ -52,13 +51,22 @@ export const NavigationBar = () => {
             className="rounded-tl-sm rounded-bl-sm rounded-tr-none rounded-br-none border-none"
           />
           <div className="flex w-12 justify-center items-center">
-            <Image
-              src="./search.svg"
-              width={32}
-              height={32}
-              alt="search icon"
-              className=""
-            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-search"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+              <path d="M21 21l-6 -6" />
+            </svg>
           </div>
         </div>
 
