111ab59e07ed5df7d1635b949a889cc8bf52d108
pankajkoree <pankajkori2057@gmail.com>
2026-01-29 19:28:21 +0545

    working on frontend 🔃

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,33 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 export default function Home() {
-  return <div></div>;
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["products-data"],
+    queryFn: async () => {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}api/products`,
+      );
+
+      return response.data;
+    },
+    staleTime: Infinity,
+    gcTime: Infinity,
+  });
+
+  return (
+    <div>
+      {isLoading ? (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      ) : (
+        <div>
+          <h1>data</h1>
+        </div>
+      )}
+    </div>
+  );
 }
